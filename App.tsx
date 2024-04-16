120bb97cf02b39a989742367c6861d18c38507e7
chiefguto <gurehem1@gmail.com>
2024-04-15 22:18:17 -0300

    tela-login-tentativa

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App() 
+{
+  
 }
 
 const styles = StyleSheet.create({
